a72aa0e5b5731c4acc0a6f20991d040cbb3672f6
reza.latifi <reza72rg@gmail.com>
2023-12-23 23:31:14 +0330

    V "1.12"
    I in this commit i use DefaultRouter for urls

--- core/todo/api/v1/urls.py
+++ core/todo/api/v1/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 from .views import * 
-
+from rest_framework.routers import DefaultRouter
 
 app_name = 'api-v1'
 
-urlpatterns = [
+router = DefaultRouter()
+router.register('task', TaskListViewSet, basename='task')
+router.register('status', StatusListModuleSet, basename='status')
+urlpatterns = router.urls
+
+
+
+
+'''urlpatterns = [
     path('task/',TaskListViewSet.as_view({'get':'list','post':'create'}), name ='task-list'),
     path('status/',StatusListModuleSet.as_view({'get':'list','post':'create'}), name ='status-list'),
     path('task/<int:pk>/',TaskListViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}), name ='task-detail'),
     path('status/<int:pk>/',StatusListModuleSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}), name ='status-detail'),
     
-]
+]'''
