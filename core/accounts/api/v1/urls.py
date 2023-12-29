584a14492396358077f27fdc56f0730c91d4617a
reza.latifi <reza72rg@gmail.com>
2023-12-29 22:39:56 +0330

    V "1.20"
    In this commit i use jwt for create access and refresh token

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 #from rest_framework.authtoken.views import obtain_auth_token
 from .views import RegisterApiViews, CustomAuthToken, CustomDiscardAuthToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 
 app_name = 'api-v1'
 
 urlpatterns = [
     path("register/", RegisterApiViews.as_view(), name="register"),
     #path('token/login', obtain_auth_token,name="token-login"),
-    path('token-custom/login', CustomAuthToken.as_view(),name="token-login"),
-    path('token/logout', CustomDiscardAuthToken.as_view(),name="token-logout"),
+    path('token-custom/login/', CustomAuthToken.as_view(),name="token-login"),
+    path('token/logout/', CustomDiscardAuthToken.as_view(),name="token-logout"),
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
 ]
--- core/core/settings.py
+++ core/core/settings.py
@@ -128,6 +128,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
        
 
     ]
