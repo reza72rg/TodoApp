5a38421c45b88c2326f20ef6adcf6005decdb7f6
reza.latifi <reza72rg@gmail.com>
2024-01-15 22:17:21 +0330

    reformatted for flake8

--- core/todo/api/v1/urls.py
+++ core/todo/api/v1/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path
+# from django.urls import path
 from .views import TaskListViewSet, StatusListModuleSet, UsersListModuleSet
 from rest_framework.routers import DefaultRouter
 
 app_name = "api-v1"
 
-'''router = DefaultRouter()
+router = DefaultRouter()
 router.register("task", TaskListViewSet, basename="task")
 router.register("status", StatusListModuleSet, basename="status")
 router.register("users", UsersListModuleSet, basename="users")
-urlpatterns = router.urls'''
-urlpatterns = [
+urlpatterns = router.urls
+"""urlpatterns = [
     path(
         "task/",
         TaskListViewSet.as_view({"get": "list", "post": "create"}),
@@ -45,4 +45,4 @@ urlpatterns = [
         name="status-detail",
     ),
 ]
-
+"""
