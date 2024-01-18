52a2b18dbfab120065e55f461213396d62d9b41b
reza.latifi <reza72rg@gmail.com>
2024-01-18 18:00:38 +0330

    reformatted for flake8
    reformatted for flake8

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -32,7 +32,9 @@ class UserCreationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if (
-            cd["password1"] and cd["password2"] and cd["password1"] != cd["password2"]
+            cd["password1"]
+            and cd["password2"]
+            and cd["password1"] != cd["password2"]
         ):
             raise ValidationError("passwords don't match")
         return cd["password2"]
--- core/core/settings.py
+++ core/core/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
