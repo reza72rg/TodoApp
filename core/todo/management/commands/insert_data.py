b89795c5c731ef0c6d4c53710c0c34703ac71ad6
reza.latifi <reza72rg@gmail.com>
2024-01-18 17:47:10 +0330

    V "1.34"
    In This commit I create 5 fake task with status Completed in databases

--- core/todo/management/commands/insert_data.py
+++ core/todo/management/commands/insert_data.py
@@ -3,11 +3,11 @@ from faker import Faker
 from accounts.models import User, Profile
 from todo.models import Status, Task
 import random
-status_list = [
+'''status_list = [
     "It",
     "Design",
     "Fun",
-]
+]'''
 
 class Command(BaseCommand):
     help = "Insert dump data"
@@ -25,13 +25,13 @@ class Command(BaseCommand):
         profile.descriptions = self.fake.paragraph(nb_sentences= 5)
         profile.save()
         
-        for item in status_list:
-            Status.objects.get_or_create(name = item)
+        '''for item in status_list:
+            Status.objects.get_or_create(name = item)'''
         
         for _ in range(10):
             Task.objects.create(
                 author = profile,
                 title = self.fake.paragraph(nb_sentences= 1),
                 description = self.fake.paragraph(nb_sentences= 5),
-                status = Status.objects.get(name=random.choice(status_list)),
+                status = Status.objects.get(name='Completed'),
             )
