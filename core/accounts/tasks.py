e392e708a9564269124bdf08c661be25190b14db
reza.latifi <reza72rg@gmail.com>
2024-01-30 17:37:57 +0330

    V "1.41"
    In this commit I Create Scheduler task that delete complete task

--- core/accounts/tasks.py
+++ core/accounts/tasks.py
@@ -1,7 +1,13 @@
 from celery import shared_task
-
+from todo.models import Task
 import time
 @shared_task
 def Send_email():
     time.sleep(5)
     print("Done Sending hi reza")
+    
+    
+@shared_task
+def Delete_task_complete():
+    response = Task.objects.filter(status = 3)
+    response.delete()
